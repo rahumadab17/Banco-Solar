cbf2addac05330e9aa49a554a9cb741fda19900a
rahumadab <148492927+rahumadab17@users.noreply.github.com>
2024-05-15 00:53:46 -0400

    avance de verbos de la API

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 const express = require('express');
-const chalk = require('chalk')
+const chalk = require('chalk');
+const { insertarUsuario, eliminarUsuario, obtenerUsuarios } = require('./queries.js');
 const app = express();
 
 const port = 3000;
 
 app.listen(port, console.log(chalk.greenBright.bold(`Server ON in port ${port}`)));
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/usuario", async (req, res) => {
+    try {
+        const { nombre, balance } = req.body;
+
+        const result = await insertarUsuario(nombre, balance);
+        res.json(result)
+    } catch (error) {
+        const { code } = error;
+        console.log(chalk.redBright.bold(`No se pudo insertar al usuario debido al error N°: ${code}`));
+        console.log(error);
+    }
+});
+
+app.get("/usuarios", async (req, res) => {
+    try {
+        const result = await obtenerUsuarios();
+        res.json(result);
+    } catch (error) {
+        const { code } = error;
+        console.log(chalk.redBright.bold(`No se pudo encontrar las canciones debido al error N°: ${code}`));
+    }
+});
+
+app.put("/usuario", async (req, res) => {
+
+});
+
+app.delete("/usuario", async (req, res) => {
+    try {
+        const { id } = req.query
+        const result = await eliminarUsuario(id)
+        res.json(result)
+    } catch (error) {
+        const { code } = error;
+        console.log(chalk.redBright.bold(`No se pudo eliminar el usuario debido al error N°: ${code}`));
+    }
+});
+
+app.post("/transferencia", async (req, res) => {
+
+});
+
+app.get("/transferencias", async (req, res) => {
+
 });
--- queries.js
+++ queries.js
@@ -3,9 +3,35 @@ const { Pool } = require('pg');
 const config = {
     host: "127.0.0.1",
     port: 5432,
-    database: "agendamiento_medico",
+    database: "bancosolar",
     user: "postgres",
     password: "7804"
 };
 
 const pool = new Pool(config);
+
+const insertarUsuario = async (nombre, balance) => {
+    const consult = {
+        text: "insert into usuarios (nombre, balance) values ($1, $2);",
+        values: [nombre, balance]
+    };
+
+    const result = await pool.query(consult);
+    return result;
+};
+
+const obtenerUsuarios = async () => {
+    const result = await pool.query("select * from usuarios;");
+    return result;
+};
+
+const eliminarUsuario = async (id) => {
+    const borrar = {
+        text: `delete from usuarios where id = '${id}'`
+    }
+
+    const result = pool.query(borrar);
+    return result
+};
+
+module.exports = { insertarUsuario, obtenerUsuarios ,eliminarUsuario }
